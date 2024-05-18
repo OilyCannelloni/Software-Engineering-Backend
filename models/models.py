308c0045fbfec56606a619bcbace43260eeabe3f
bartn1k <bartek.slupik@gmail.com>
2024-05-18 14:59:52 +0200

    question type

--- models/models.py
+++ models/models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pydantic import BaseModel
 from abc import ABC, abstractmethod
 
@@ -12,26 +13,33 @@ class User(BaseModel):
         return self.name == other.name
 
 
+class QuestionType(Enum):
+    TextBox = 1
+    MultipleChoice = 2
+
+
 class Question(BaseModel, ABC):
     text: str
+    type: QuestionType
 
     @abstractmethod
     def get_answer_text(self) -> str:
         pass
 
     @abstractmethod
-    def get_answer_details(self) -> dict[str, ...]:
+    def get_answer_details(self):
         pass
 
 
 class TextBoxQuestion(Question):
+    type = QuestionType.TextBox
     textbox_content: str
 
     def get_answer_text(self) -> str:
         return self.textbox_content
 
-    def get_answer_details(self) -> dict[str, ...]:
-        return {"type": "textbox", "value": self.textbox_content}
+    def get_answer_details(self):
+        return self.textbox_content
 
 
 class QuestionOption(BaseModel):
@@ -43,13 +51,14 @@ class MultipleChoiceQuestion(Question):
     """
     For now, use this to model single choice questions as well.
     """
+    type = QuestionType.MultipleChoice
     options: list[QuestionOption]
 
     def get_answer_text(self):
         return "\n".join(opt.text for opt in self.options if opt.is_chosen)
 
-    def get_answer_details(self) -> dict[str, ...]:
-        return {"type": "multiple_choice", "value": [i for i, opt in enumerate(self.options) if opt.is_chosen]}
+    def get_answer_details(self) -> list[int]:
+        return [i for i, opt in enumerate(self.options) if opt.is_chosen]
 
 
 class Poll(BaseModel):
