e4b7a9119ac525abe58d6790e2e849f3fae7bdf6
Uiskam <maksulima@gmail.com>
2024-05-22 23:34:11 +0200

    Add /start-game and /end-game endpoints and modify /register.

--- core/game.py
+++ core/game.py
@@ -1,14 +1,30 @@
+import asyncio
 from models.models import User
+from fastapi import Request
 
 
 class Game:
-    user_data: dict[User] = {}
+    user_data: dict[User, asyncio.Queue] = {}
 
-    def register_user(self, user: User):
+    def register_user(self, user: User, request: Request):
         if user in self.user_data.keys():
             return False
-        self.user_data[user] = {}
-        return True
+
+        self.user_data[user] = asyncio.Queue()
+
+        async def event_generator():
+            try:
+                while True:
+                    # Wait for a new message to be available in the queue
+                    message = await self.user_data[user].get()
+                    yield message
+                    # If the client closes the connection, we break the loop
+                    if await request.is_disconnected():
+                        break
+            except asyncio.CancelledError:
+                pass
+
+        return event_generator()
 
     def remove_user(self, user: User):
         if user in self.user_data.keys():
@@ -18,3 +34,12 @@ class Game:
 
     def remove_user_by_name(self, name: str):
         return self.remove_user(User(name=name))  # this works because users are hashed by user.name
+
+    def start_game(self):
+        for queue in self.user_data.values():
+            queue.put_nowait("start\n\n")
+
+    def end_game(self):
+        for queue in self.user_data.values():
+            queue.put_nowait("end\n\n")
+
